5294f03c65d4e4f6500cd6fed22b76e1379fc753
ccgsisig <87790430+ccgsisig@users.noreply.github.com>
2024-01-25 11:39:03 -0600

    i am tired

--- image_filtering.py
+++ image_filtering.py
@@ -9,22 +9,24 @@ import matplotlib.pyplot as plt
 # main function
 if __name__ == '__main__':
     # Step 1: read the crack box image with cv2.imread
-    im = None
+    im = cv2.imread('cracker_box.jpg')
 
 
     # Step 2: use cv2.cvtColor to convert RGB image to gray scale image 
     #(replace with your code)
-    gray_image = None
+    gray_image = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
 
     # Step 3: define the filter kernel as described in the homework description as a numpy array
     #(replace with your code)
-    kernel = None
+    kernel = np.array([[0, -1, 0],
+                       [-1, 5, -1],
+                       [0, -1, 0]])
 
 
     # Step 4: filter the image with the kernel
     #(replace with your code)
-    output = None
+    output = cv2.filter2D(gray_image, -1, kernel)
 
 
     # show result with matplotlib (no need to change code below)
